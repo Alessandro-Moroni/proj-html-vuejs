06a365037be484bdd1537ba9c0c0b30ead629ce1
Alessandro-Moroni <alemoro98@gmail.com>
2024-04-09 13:17:03 +0200

    [starting]main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -14,12 +14,48 @@ import { store } from '../data/store';
 </script>
 
 <template>
-  <div class="container">
-    main
+  <div class="container under">
+    
+    <div class="container above d-flex justify-content-around">
+      <div>
+        <i class="fa-regular fa-clock"></i>24/7 DELIVERY
+      </div>
+
+      <div>
+        <i class="fa-solid fa-burger"></i>OVER 100 DISHES
+      </div>
+
+      <div>
+        <i class="fa-solid fa-mobile-screen-button"></i>IN APP ORDERING
+      </div>
+
+      <div>
+        <i class="fa-solid fa-car"></i>FAST DELIVERY
+      </div>
+      
+    </div>
   </div>
 </template>
 
 
 <style lang="scss" scoped>
-
+.under{
+  position: relative;
+  height: 1000px;
+}
+.above{
+  position: absolute;
+  background-color: white;
+  padding-top: 50px;
+  top: -35px;
+  left: 50%;
+  transform: translateX(-50%);
+  .fa-solid{
+    margin-right: 20px;
+  }
+  .fa-regular{
+    margin-right: 20px;
+  }
+  
+}
 </style>
