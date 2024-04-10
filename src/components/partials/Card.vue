0ae0f4b921aa9e8871175a33d68894c375edd73b
Alessandro-Moroni <alemoro98@gmail.com>
2024-04-10 18:00:59 +0200

    [fix]hover-card

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -19,6 +19,11 @@ import { cardmain } from '../../data/cardmain.js';
     <img :src="item.image" alt="image">
     <h3>{{ item.title }}</h3>
     <p>{{ item.price }}</p>
+    <div class="check">
+      <i class="fa-regular fa-circle-check"></i>
+        <div>VIEW CART</div>
+    </div>
+
   </div>
 </template>
 
@@ -26,6 +31,31 @@ import { cardmain } from '../../data/cardmain.js';
 <style lang="scss" scoped>
 .card{
   border: none;
+  position: relative;
+  cursor: pointer;
+  .check{
+    position: absolute;
+    color: white;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%);
+    display: none;
+    .fa-circle-check{
+      border-radius: 50%;
+      height: 100px;
+      width: 100px;
+      font-size: 4rem;
+      line-height: 100px;
+      background-color: rgb(69, 68, 68, .5);
+    }
+    div{
+      font-weight: bold;
+    }
+    
+  }
+  &:hover .check{
+    display: block;
+  }
 }
 img{
   margin-bottom: 30px;
